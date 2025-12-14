45561fcd8a9e5bed976897cb0f9f859da42c9cc7
choiminwoo02-lgtm <choiminwoo02@gmail.com>
2025-12-14 15:20:39 +0900

    Add CalendarDay class for date representation

---
+++ CalenderDay.dart
@@ -0,0 +1,16 @@
+
+class CalendarDay {
+  final DateTime date;
+  final int day;
+  final bool isCurrentMonth;
+  final bool isToday;
+  final bool hasWorkout;
+
+  CalendarDay({
+    required this.date,
+    required this.day,
+    required this.isCurrentMonth,
+    required this.isToday,
+    required this.hasWorkout,
+  });
+}
